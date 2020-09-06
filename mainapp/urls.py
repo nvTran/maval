7f191fdd48413e918ccf994206bcbaa86dee1ab6
Minh <54620258+ntuanminh3108@users.noreply.github.com>
2020-09-06 16:20:22 +0800

    urls for production

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from . import views 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,3 +14,6 @@ urlpatterns = [
     path('risk/',views.risk, name='risk'),
     path('register/',views.register, name='register')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
